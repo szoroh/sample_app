cfab2196b6a307b1b011dc8a621a3bb29d1ae2e6
Jakub Horosz <kubahorosz95@gmail.com>
2018-10-28 14:57:44 +0100

    Finish user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -10,6 +10,11 @@ end
 gem 'rails', '~> 5.1.6'
 #bcrypt 
 gem 'bcrypt', '3.1.12'
+# faker
+gem 'faker', '1.7.3'
+#paginate 
+gem 'will_paginate', '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
 #bootstrap-add
 gem 'bootstrap-sass', '3.3.7'
 # Use sqlite3 as the database for Active Record
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,29 @@
 module SessionsHelper
-    # Logs in the given user.
+    # logs in the given user.
     def log_in(user)
       session[:user_id] = user.id
     end
-
+    
+    # remembers a user in persistent session
     def remember(user)
       user.remember
       cookies.permanent.signed[:user_id] = user.id
       cookies.permanent[:remember_token] = user.remember_token
-    end  
-    # Returns the current logged-in user (if any).
+    end 
+    
+    # remembers a user in a persistent session
+    def remember(user)
+      user.remember
+      cookies.permanent.signed[:user_id] = user.id
+      cookies.permanent[:remember_token] = user.remember_token
+    end
+
+    # returns true if the given user is the current user
+    def current_user?(user)
+      user == current_user
+    end
+
+    # returns the current logged-in user (if any)
     def current_user
       if (user_id = session[:user_id])
         @current_user ||= User.find_by(id: user_id)
@@ -22,7 +36,7 @@ module SessionsHelper
       end
     end
   
-    # Returns true if the user is logged in, false otherwise.
+    # returns true if the user is logged in, false otherwise
     def logged_in?
       !current_user.nil?
     end
@@ -38,4 +52,15 @@ module SessionsHelper
       session.delete(:user_id)
       @current_user = nil
     end 
+
+    # redirects to stored location / or to the default
+    def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+    end
+
+    # stores the URL trying to be accessed
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
+    end
 end
